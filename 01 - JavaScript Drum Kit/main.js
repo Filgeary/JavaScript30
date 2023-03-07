21d4cc7c55a6602fc24c10a7b770d9dc91469ba8
Georgiy Filippov <g.filippov777@gmail.com>
2023-03-08 02:35:08 +0500

    01 fixes

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,19 +1,24 @@
 'use strict'
 
 window.addEventListener('DOMContentLoaded', () => {
-  const keysElems = [...document.querySelectorAll('.key')]
-  const audioElems = [...document.querySelectorAll('audio')]
-  let timer
+  const keysList = [...document.querySelectorAll('.key')]
+  const audioList = [...document.querySelectorAll('audio')]
+
+  keysList.forEach(elem => {
+    elem.addEventListener('transitionend', evt => {
+      if (evt instanceof TransitionEvent) {
+        if (evt.propertyName !== 'transform') return
+        if (evt.target instanceof HTMLElement) evt.target.classList.remove('playing')
+      }
+    })
+  })
 
   window.addEventListener('keydown', evt => {
-    if (timer) clearTimeout(timer)
-
-    const keyElem = keysElems.find(elem => elem.getAttribute('data-key') === evt.code)
+    const keyElem = keysList.find(elem => elem.getAttribute('data-key') === evt.code)
     keyElem?.classList.add('playing')
 
-    const audioElem = audioElems.find(elem => elem.getAttribute('data-key') === evt.code)
+    const audioElem = audioList.find(elem => elem.getAttribute('data-key') === evt.code)
+    if (audioElem) audioElem.currentTime = 0
     audioElem?.play()
-
-    timer = setTimeout(() => keysElems.forEach(elem => elem.classList.remove('playing')), 300)
   })
 })
